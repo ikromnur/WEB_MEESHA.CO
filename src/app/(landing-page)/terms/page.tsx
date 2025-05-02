6c701674f507857b08433ec03b07727f7976e1aa
Muhammad Faiq Mubarok <faiqmubarok@gmail.com>
2025-05-03 04:57:53 +0700

    feat: create page term of services

---
+++ src/app/(landing-page)/terms/page.tsx
@@ -0,0 +1,11 @@
+import TermsPage from "@/components/pages/landing-page/term";
+
+export const metadata = {
+  title: "Term Of Service",
+};
+
+const Term = () => {
+  return <TermsPage />;
+};
+
+export default Term;
