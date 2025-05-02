6755a5968c4a6d3a9701c28b04451d5bce5a1b5c
Muhammad Faiq Mubarok <126318140+faiqmubarok@users.noreply.github.com>
2025-05-03 05:21:04 +0700

    feat: create not found page (#9)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import NotFoundPage from "@/components/pages/not-found";
+
+export const metadata = {
+  title: "404 - Page Not Found",
+};
+
+export default function NotFound() {
+  return <NotFoundPage />;
+}
---
+++ src/components/pages/not-found.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+
+const NotFoundPage = () => {
+  const router = useRouter();
+
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center px-6 py-12 text-center bg-white">
+      <div className="max-w-md w-full space-y-6">
+        <div className="relative w-full h-64">
+          <Image
+            src="/404-illustration.svg"
+            alt="404 Illustration"
+            layout="fill"
+            objectFit="contain"
+            priority
+          />
+        </div>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          Oops! Page Not Found
+        </h1>
+        <p className="text-gray-600">
+          The page you&apos;re looking for doesn&apos;t exist or has been moved.
+        </p>
+        <Button size={"lg"} onClick={() => router.push("/")}>
+          Return to Homepage
+        </Button>
+      </div>
+    </main>
+  );
+};
+
+export default NotFoundPage;
