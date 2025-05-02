014567fe44fe2df40d51c1a242dda82bb9576239
Muhammad Faiq Mubarok <faiqmubarok@gmail.com>
2025-05-03 05:03:43 +0700

    feat: create privacy policy page and update logo on footer link to root

---
+++ src/app/(landing-page)/privacy/page.tsx
@@ -0,0 +1,11 @@
+import PrivacyPage from "@/components/pages/landing-page/privacy";
+
+export const metadata = {
+  title: "Privacy Policy",
+};
+
+const PrivacyPolicy = () => {
+  return <PrivacyPage />;
+};
+
+export default PrivacyPolicy;
